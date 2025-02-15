4378d202e27248f3cf05515c434b38eb1c1c9ffd
advanced-go <mark.wilson.go@outlook.com>
2025-02-15 13:13:12 -0600

    Update resolver.go

--- collective/resolver.go
+++ collective/resolver.go
@@ -1,11 +1,5 @@
 package collective
 
-/*
-type resolver interface {
-	resolve(name string, version int) ([]byte, error)
-}
-*/
-
 type contentResolver func(name string, version int) ([]byte, error)
 
 type relationResolver func(name string) (relation, error)
