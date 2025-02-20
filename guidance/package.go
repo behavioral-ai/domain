1b7edffa1d6f5dcff3123f0cd068a80e19fea15c
advanced-go <mark.wilson.go@outlook.com>
2025-02-20 17:50:18 -0600

    Updating

--- guidance/package.go
+++ guidance/package.go
@@ -1,31 +1,5 @@
 package guidance
 
-import (
-	"github.com/behavioral-ai/core/messaging"
-	"github.com/behavioral-ai/domain/common"
-)
-
 const (
-	PkgPath    = "github/behavioral-ai/domain/guidance"
-	WestRegion = "us-west1"
-	WestZoneA  = "w-a"
-	WestZoneB  = "w-b"
-
-	CentralRegion = "us-central1"
-	CentralZoneA  = "c-a"
-	CentralZoneB  = "c-b"
-
-	EastRegion = "us-east1"
-	EastZoneA  = "e-a"
-	EastZoneB  = "e-b"
+	PkgPath = "github/behavioral-ai/domain/guidance"
 )
-
-func GetRegion(origin common.Origin) ([]HostEntry, *messaging.Status) {
-	if origin.Region == WestRegion {
-		return westData, messaging.StatusOK()
-	}
-	if origin.Region == CentralRegion {
-		return centralData, messaging.StatusOK()
-	}
-	return []HostEntry{}, messaging.StatusOK()
-}
