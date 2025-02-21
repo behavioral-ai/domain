d25a97266c59d4a9705752df8ba34adeddfe6ba8
advanced-go <mark.wilson.go@outlook.com>
2025-02-21 07:27:07 -0600

    Update package.go

--- common/package.go
+++ common/package.go
@@ -2,14 +2,14 @@ package common
 
 const (
 	WestRegion = "us-west1"
-	WestZoneA  = "w-a"
-	WestZoneB  = "w-b"
+	WestZoneA  = "w-zone-a"
+	WestZoneB  = "w-zone-b"
 
 	CentralRegion = "us-central1"
-	CentralZoneA  = "c-a"
-	CentralZoneB  = "c-b"
+	CentralZoneA  = "c-zone-a"
+	CentralZoneB  = "c-zone-b"
 
 	EastRegion = "us-east1"
-	EastZoneA  = "e-a"
-	EastZoneB  = "e-b"
+	EastZoneA  = "e-zone-a"
+	EastZoneB  = "e-zone-b"
 )
