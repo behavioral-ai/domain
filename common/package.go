95123469f27cef2d8cb5cdc39eb0d451d74bbd88
advanced-go <mark.wilson.go@outlook.com>
2025-02-20 18:03:12 -0600

    Create package.go

---
+++ common/package.go
@@ -0,0 +1,15 @@
+package common
+
+const (
+	WestRegion = "us-west1"
+	WestZoneA  = "w-a"
+	WestZoneB  = "w-b"
+
+	CentralRegion = "us-central1"
+	CentralZoneA  = "c-a"
+	CentralZoneB  = "c-b"
+
+	EastRegion = "us-east1"
+	EastZoneA  = "e-a"
+	EastZoneB  = "e-b"
+)
