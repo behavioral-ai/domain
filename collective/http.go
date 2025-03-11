cd9a6c6f00223bb15c3652e42e467101ca37ce1f
advanced-go <mark.wilson.go@outlook.com>
2025-03-11 18:55:12 -0500

    Update http.go

--- collective/http.go
+++ collective/http.go
@@ -2,10 +2,14 @@ package collective
 
 import (
 	"github.com/behavioral-ai/core/messaging"
+	"net/http"
 )
 
 func httpResolution(method, name, author string, body []byte, version int) ([]byte, *messaging.Status) {
 	//req, _ := http.NewRequest(method, "", nil)
 	//_, _ := http.DefaultClient.Do(req)
-	return nil, messaging.StatusOK()
+	if method == http.MethodPut {
+		return nil, messaging.StatusOK()
+	}
+	return nil, messaging.StatusNotFound()
 }
